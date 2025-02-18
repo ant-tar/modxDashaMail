b48141a8c0e98da47d0d09d04ccca3ab8d69d956
Anton Tarasov <himurovich@gmail.com>
2025-02-18 16:42:58 +0400

    minor fix

--- core/components/dashamail/elements/plugins/dmMsOrderUserSubscribe.php
+++ core/components/dashamail/elements/plugins/dmMsOrderUserSubscribe.php
@@ -11,21 +11,15 @@ switch ($modx->event->name) {
       $msPhone = $msAddress->get('phone');
       $msEmail = $msAddress->get('email');
       $msAddressReceiver = $msAddress->get('receiver');
-      $modx->log(modX::LOG_LEVEL_ERROR, $msAddressReceiver, '', 'sendOrder');       
-      $modx->log(modX::LOG_LEVEL_ERROR, $msEmail, '', 'sendOrder');        
-      $modx->log(modX::LOG_LEVEL_ERROR, $msPhone, '', 'sendOrder');        
+ 
       if($msSubscription){
-          $modx->log(modX::LOG_LEVEL_ERROR, 'checkbox is active', '', 'sendOrder');    
         $dashamail = $modx->getService('dashamail','DashaMail',$modx->getOption('dashamail.core_path',null,$modx->getOption('core_path').'components/dashamail/').'model/dashamail/',[]);
         if (!($dashamail instanceof DashaMail)) {
             return;
         }
         $dm = new DashaMail($modx);
         $mergeData = ['merge_3' => $msPhone]; //
-        $modx->log(modX::LOG_LEVEL_ERROR, 'merge params='.print_r($mergeData,true), '', 'sendOrder'); 
         $listMember = $dm->addListMember($dmListID, $msEmail, $mergeData);
-        $modx->log(modX::LOG_LEVEL_ERROR, 'data sent to DM', '', 'sendOrder');    
       }
-   
-       break;
+    break;
 }
