8382b92cb29af5722bda7652ef39476334bf9ed0
Anton Tarasov <himurovich@gmail.com>
2025-03-04 23:17:13 +0400

    preparations for 1.0.2-pl

--- core/components/dashamail/lexicon/en/setting.inc.php
+++ core/components/dashamail/lexicon/en/setting.inc.php
@@ -8,8 +8,8 @@ $_lang['setting_dashamail_api_key_desc'] = 'Take it from DashaMail website setti
 $_lang['setting_dashamail_list_id'] = 'User list ID';
 $_lang['setting_dashamail_list_id_desc'] = 'Take it from DashaMail website, basically this is list ID from page URL';
 
-$_lang['setting_dashamail_merge_list'] = 'Default merge list';
-$_lang['setting_dashamail_list_id_desc'] = 'If not redefined this one will be used by default';
+$_lang['setting_dashamail_merge_fields'] = 'Default merge list';
+$_lang['setting_dashamail_merge_fields_desc'] = 'If not redefined this one will be used by default';
 
 $_lang['setting_dashamail_checkbox_name'] = 'Checkbox field name';
 $_lang['setting_dashamail_list_id_desc'] = 'Checkbox name for subscription confirmation, empty value means no confirmation';
